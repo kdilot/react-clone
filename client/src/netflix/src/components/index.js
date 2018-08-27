3ea150bc24a5b36496f76f6ddb0ae941eb852fb5
kdilot <kdilot@gmail.com>
2018-08-26 17:32:54 -0700

    view list

---
+++ client/src/netflix/src/components/index.js
@@ -0,0 +1,6 @@
+export {default as MainController} from './MainController'
+export {default as Modal} from './Modal'
+export {default as ItemList} from './ItemList'
+export {default as SideMenu} from './SideMenu'
+export {default as SliderButton} from './SliderButton'
+export {default as Profile} from './Profile'
